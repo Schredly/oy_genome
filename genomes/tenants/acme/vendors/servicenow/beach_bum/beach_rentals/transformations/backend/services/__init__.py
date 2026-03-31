306d1dead6da0a768943fe31e534f8db15a01c2b
Schredly Schrederson <144973458+Schredly@users.noreply.github.com>
2026-03-31 08:02:36 -0700

    Genome Studio: create transformations/backend/services/__init__.py

---
+++ genomes/tenants/acme/vendors/servicenow/beach_bum/beach_rentals/transformations/backend/services/__init__.py
@@ -0,0 +1,5 @@
+# This package provides the service layer for the Beach Rentals application.
+# Business logic related to various processes and operations will be implemented here,
+# with each process or operation having a dedicated module for organization.
+
+__all__ = ["logic", "workflows"]
