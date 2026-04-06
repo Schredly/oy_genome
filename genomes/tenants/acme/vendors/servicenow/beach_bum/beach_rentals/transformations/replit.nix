fb80b818323175d313b335486d7f703054bb7d0b
Schredly Schrederson <144973458+Schredly@users.noreply.github.com>
2026-04-06 15:03:33 -0700

    Genome Studio: create transformations/replit.nix

--- genomes/tenants/acme/vendors/servicenow/beach_bum/beach_rentals/transformations/replit.nix
+++ genomes/tenants/acme/vendors/servicenow/beach_bum/beach_rentals/transformations/replit.nix
@@ -4,5 +4,9 @@
     pkgs.python311Packages.fastapi
     pkgs.python311Packages.uvicorn
     pkgs.python311Packages.pydantic
+    pkgs.python311Packages.aiofiles
+    pkgs.python311Packages.jinja2
+    pkgs.python311Packages.httpx
+    pkgs.python311Packages.sqlalchemy
   ];
 }
